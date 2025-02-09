c069689099440aa596e2b9e57495b6051233854f
stufuuu <98703856+kaktu4ik@users.noreply.github.com>
2025-02-09 04:58:12 +0200

    Add files via upload

---
+++ cowsay/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace cowsay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("cowsay test --> ");
+            string userText = Console.ReadLine();
+
+            Console.Clear();
+
+            cowsay(userText);
+
+            Console.ReadLine();
+        }
+
+        static void cowsay(string Print)
+        {
+            Console.Write("  ");
+            for (int i = 0; i < Print.Length; i++)
+            {
+                Console.Write("_");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("| " + Print + " |");
+
+            Console.Write("  ");
+            for (int i = 0; i < Print.Length; i++)
+            {
+                Console.Write("=");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("          \\");
+            Console.WriteLine("           \\");
+            Console.WriteLine("             ^__^");
+            Console.WriteLine("             (oo)\\_______");
+            Console.WriteLine("             (__)\\       )\\/\\");
+            Console.WriteLine("                 ||----w |");
+            Console.WriteLine("                 ||     ||");
+        }
+    }
+}
